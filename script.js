c3f3e6f92cbf77dde1154c1cc22f39dec43c2549
Leonardo Almeida <leo@leleo.com.br>
2023-09-18 22:09:19 +0200

    fix mode button, changed close button color, made everything responsive

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ let toggleMode = () => {
         theme = 'dark'
         document.body.style.backgroundColor = '#000'
         document.querySelectorAll("#bio")[0].style.color = '#dcb'
+        document.querySelectorAll("#moon")[0].style.display = 'none'
+        document.querySelectorAll("#sun")[0].style.display = 'block'
 
     } else {
         theme = 'light'
         document.body.style.backgroundColor = '#fff'
         document.querySelectorAll("#bio")[0].style.color = 'rgb(106, 99, 97)'
         console.log(document.querySelectorAll(".bio"));
+        document.querySelectorAll("#sun")[0].style.display = 'none'
+        document.querySelectorAll("#moon")[0].style.display = 'block'
+
 
 
     }
@@ -23,12 +28,31 @@ let toggleMode = () => {
 
 
 let imageBehavior = e => {
-    document.querySelector(".grid").style.display = 'none'
+    document.querySelector("#grid").style.display = 'none'
     document.querySelectorAll(".project")[e].style.display = 'flex'
     console.log(e);
 }
 
+let closeBehavior = e => {
+    document.querySelector("#grid").style.display = 'block'
+    document.querySelectorAll(".project")[e].style.display = 'none'
+    console.log(e);
+
+}
+
+
+
 window.onload = () => {
+    var buttonCollection = document.getElementsByClassName("proj-close");
+    var buttonItems = [...buttonCollection]
+
+    buttonItems.forEach(function (element, i) {
+        element.addEventListener("click", function () {
+            closeBehavior(i);
+        }
+        );
+    });
+
 
     var gridCollection = document.getElementsByClassName("grid-item");
     var gridItems = [...gridCollection]
