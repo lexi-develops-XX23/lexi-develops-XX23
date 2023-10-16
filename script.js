3d404c1e6661d59be4bcad78c2df899ffca2b461
graphicMatters <simonida@hotmail.nl>
2023-10-16 13:56:12 +0200

    trying to upload video into banner _ MIME type does not support video file

--- script.js
+++ script.js
@@ -64,3 +64,10 @@ window.onload = () => {
         );
     });
 }
+
+MediaSource.isTypeSupported()
+
+function canPlayH264 () {
+    var v = document.createElement('video');
+    return !!(v.canPlayType && v.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"').replace(/no/, ''));
+};
